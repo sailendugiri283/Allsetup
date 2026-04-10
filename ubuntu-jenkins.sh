a8b6c0059cce5f2e104d4e38bd874142506e812e
sailendugiri283 <sailendugiri283@gmail.com>
2026-04-10 12:23:40 +0530

    Create ubuntu-jenkins.sh

---
+++ ubuntu-jenkins.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+sudo apt update -y
+sudo apt install -y openjdk-17-jdk
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
+
+sudo systemctl start jenkins
+sudo systemctl status jenkins
